a56f624f6661430692fed9d93e8c960204f4be81
Bikash74 <shaw46482@gmail.com>
2023-12-08 23:40:32 +0530

    first program in java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package Java_Practice;
+
+/**
+ * helloworld
+ */
+public class helloworld {
+     public static void main(String[] args) {
+           System.out.println("hello world");
+     }
+    
+}
