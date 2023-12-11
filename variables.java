cdd90a1dcb3e518104a19fdcb109934e2be9aaad
Bikash74 <shaw46482@gmail.com>
2023-12-11 22:40:10 +0530

    practce datatypes in java

---
+++ variables.java
@@ -0,0 +1,16 @@
+package Java_Practice;
+
+public class variables {
+    public static void main(String[] args) {
+        byte a = 5,b=11;
+     //   byte c= a * b;
+        float d=b/2f;
+        double e=8.9;
+        long x=Long.MAX_VALUE;
+        float y= x;
+        float ab=2.5f;
+        float cd=3.5f;
+        float xy=ab*cd;
+        System.out.println(y);
+    }
+}
